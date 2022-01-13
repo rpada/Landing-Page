ec9acdd0250762cca015e5776334682ac3f832f8
rpada <70246093+rpada@users.noreply.github.com>
2022-01-12 14:08:57 -1000

    readme changes

--- Landing Page/js/app.js
+++ Landing Page/js/app.js
@@ -11,17 +11,17 @@
  * 
  * JS Standard: ESlint
  * 
-*/
+ */
 
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
+ */
 
 /**
  * Define Global Variables
  * 
-*/
+ */
 const navbarSections = document.querySelectorAll('section');
 const a = document.createElement('a'); // creates 'a' tag
 const li = document.createElement('li'); // creates 'li' tag
@@ -30,19 +30,19 @@ const li = document.createElement('li'); // creates 'li' tag
  * End Global Variables
  * Start Helper Functions
  * 
-*/
+ */
 
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
-*/
+ */
 
 // build the nav
-function buildNav(){
-    for (let i in navbarSections){ // loop through all the sections on the page
-        const sectionName = navbarSections[i].getAttribute('data-nav')
-        const sectionID = navbarSections[i].getAttribute('id')
+function buildNav() {
+    for (i of navbarSections) { // loop through all the sections on the page
+        const sectionName = i.getAttribute('data-nav')
+        const sectionID = i.getAttribute('id')
         // from Udacity 'Creating Content with Javascript' section 3, 'Add New Page Content'
         // also referenced from https://stackoverflow.com/questions/21977349/javascript-cant-add-href-to-list-item
         const a = document.createElement('a'); // creates 'a' tag
@@ -51,16 +51,19 @@ function buildNav(){
         a.appendChild(linkText); //attaches text to the a tag
         a.setAttribute('href', '#' + sectionID); // assigns values to the a tag, hash is necessary to link within page
         li.classList.add('menu__link'); // referenced from https://www.w3schools.com/howto/howto_js_add_class.asp
-        a.classList.add('menu__link')  // referenced from https://www.w3schools.com/howto/howto_js_add_class.asp
+        a.classList.add('menu__link') // referenced from https://www.w3schools.com/howto/howto_js_add_class.asp
         li.appendChild(a); // adds the a tag into the li tag
-        document.getElementById('navbar__list').appendChild(li); // adds the li tag into the unordered list with id 'navbar__list'
+
+        document.getElementById('navbar__list').appendChild(li); // adds the li tag into the unordered list with id 'navbar__list'.
+        scroll(li, i) // with help from https://knowledge.udacity.com/questions/779725
     }
 }
+
 
 // Add class 'active' to section when near top of viewport
 
 //checks if element is in the viewport
-var isInViewport = function (elem) { // I got this function from https://gomakethings.com/how-to-test-if-an-element-is-in-the-viewport-with-vanilla-javascript/
+var isInViewport = function(elem) { // I got this function from https://gomakethings.com/how-to-test-if-an-element-is-in-the-viewport-with-vanilla-javascript/
     var bounding = elem.getBoundingClientRect();
     return (
         bounding.top >= 0 &&
@@ -74,48 +77,45 @@ var isInViewport = function (elem) { // I got this function from https://gomaket
 document.addEventListener('scroll', settingActiveClass);
 
 // sets "your-active-class" if the element is inside of the viewport
-function settingActiveClass(){
-    if (isInViewport(section1)){ // if section 1 is in the viewport
+function settingActiveClass() {
+    if (isInViewport(section1)) { // if section 1 is in the viewport
         section1.classList.add("your-active-class"); // add the class
-    } else { 
+    } else {
         section1.classList.remove("your-active-class"); // otherwise remove the class
     }
-    if (isInViewport(section2)){  // if section 2 is in the viewport
+    if (isInViewport(section2)) { // if section 2 is in the viewport
         section2.classList.add("your-active-class"); // add the class
-    }
-    else {
+    } else {
         section2.classList.remove("your-active-class"); // otherwise remove the class
     }
-    if (isInViewport(section3)){  // if section 3 is in the viewport
-        section3.classList.add("your-active-class");// add the class
-    }
-    else {
+    if (isInViewport(section3)) { // if section 3 is in the viewport
+        section3.classList.add("your-active-class"); // add the class
+    } else {
         section3.classList.remove("your-active-class"); // otherwise remove the class
     }
-    if (isInViewport(section4)){  // if section 4 is in the viewport
-        section4.classList.add("your-active-class");// add the class
-    }
-    else {
-        section4.classList.remove("your-active-class");// otherwise remove the class
+    if (isInViewport(section4)) { // if section 4 is in the viewport
+        section4.classList.add("your-active-class"); // add the class
+    } else {
+        section4.classList.remove("your-active-class"); // otherwise remove the class
     }
 }
 
-// Scroll to anchor ID using scrollTO event
-// "base" code here taken from https://knowledge.udacity.com/questions/778965
 //build a scroll function
-function scroll(a, navbarSections){
-	a.addEventListener("click", function(e){
-		e.preventDefault();
-		navbarSections.scrollIntoView({behavior: "smooth"})
-	})
+// with help from Udacity mentor https://knowledge.udacity.com/questions/779725 and https://stackoverflow.com/questions/7717527/smooth-scrolling-when-clicking-an-anchor-link
+function scroll(a, section) {
+    a.addEventListener("click", function(e) {
+        e.preventDefault();
+        section.scrollIntoView({
+            behavior: "smooth"
+        })
+    })
 }
 
 /**
  * End Main Functions
  * Begin Events
  * 
-*/
+ */
 
 
 buildNav()
-
---
+++ Landing Page/readme.md
@@ -0,0 +1,24 @@
+# Landing Page Project
+
+## Table of Contents
+
+* [Instructions](#instructions)
+
+## Description
+
+This is an interactive landing page built off of the starter code provided by Udacity. Click one of the links in the navbar to scroll down to the element on the page. Each section becomes an active class once it is scrolled to. 
+
+Dependencies: None
+
+## Resources Used
+
+https://stackoverflow.com/questions/21977349/javascript-cant-add-href-to-list-item
+https://www.w3schools.com/howto/howto_js_add_class.asp
+https://gomakethings.com/how-to-test-if-an-element-is-in-the-viewport-with-vanilla-javascript/
+https://knowledge.udacity.com/questions/85408
+https://knowledge.udacity.com/questions/779725
+https://stackoverflow.com/questions/7717527/smooth-scrolling-when-clicking-an-anchor-link
+
+Udacity:
+'Creating Content with Javascript' section 3, 'Add New Page Content'
+'Working With Browser Events' 'Respond to Events'
